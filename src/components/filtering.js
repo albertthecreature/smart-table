d2aef60b8f53933ad6d9e2447925c7eef4b5a544
Albert Valeyev <albertthecreature@icloud.com>
2026-03-16 12:07:47 +0300

    Выполнена ПР6

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,37 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
+import { createComparison, defaultRules } from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
 
+const compare = createComparison(defaultRules);
+
 export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
+  // @todo: #4.1 — заполнить выпадающие списки опциями
 
-    return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
+  Object.keys(indexes).forEach((elementName) => {
+    elements[elementName].append(
+      ...Object.values(indexes[elementName]).map((name) => {
+        const option = document.createElement("option");
+        option.value = name;
+        option.textContent = name;
+        return option;
+      }),
+    );
+  });
 
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+  return (data, state, action) => {
+    // @todo: #4.2 — обработать очистку поля
+
+    const clearButton = document.querySelector('button[name="clear"]')
+    if (clearButton.action) {
+      clearButton.parentElement.querySelector('input').value = ''
+      state[clearButton.dataset.field] = ''
     }
+
+
+    // @todo: #4.5 — отфильтровать данные используя компаратор
+
+    return data.filter(row => compare(row, state));
+
+    return data;
+  };
 }
